c154b22de655d7b0a634f9bd02403c495f9df126
DJ Spiess <dspiess@nso.edu>
2017-04-18 11:50:56 -0600

    Initial Commit

---
+++ src/main/java/com/deegeu/aws/App.java
@@ -0,0 +1,13 @@
+package com.deegeu.aws;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
